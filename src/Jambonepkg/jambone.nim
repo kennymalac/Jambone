2a0dd699873a52ee45b96caa3bd6d14e6aaef4f2
Kenneth Malac <kennethm777@gmail.com>
2021-08-28 18:03:55 -0400

    Added identifier/keyword distinction to Tokenizer, beginning of parser

--- src/Jambonepkg/jambone.nim
+++ src/Jambonepkg/jambone.nim
@@ -1,6 +1,8 @@
 import json
 import tables
+import deques
 import strutils
+import strformat
 import streams
 import options
 
@@ -16,7 +18,7 @@ type
 
 # TODO functions FunctionNameToken, FunctionStartParamsToken, FunctionEndParamsToken, FunctionParamToken,
 type
-  Token* {.pure.} = enum
+  KeywordTokenKind* {.pure.} = enum
     StartExpression = "{{",
     EndExpression = "}}",
     Show = "$",
@@ -25,6 +27,19 @@ type
     IfBlock = "if",
     ElseBlock = "else",
     EndIfBlock = "endif"
+
+  TokenKind* = enum
+    jamKeyword,
+    jamIdentifier,
+    jamLiteral,
+    jamComment
+
+  Token* = object
+    case kind*: TokenKind
+      of jamKeyword: keyword*: KeywordTokenKind
+      of jamIdentifier: identifier: string
+      of jamLiteral: literal: string
+      of jamComment: comment: string
 
   TokenOccurrence* = tuple[pos: int, token: Token]
 
@@ -43,7 +58,7 @@ type
 
   JamboneASTNode = ref object
     case kind: JamboneASTKind
-    of jamRoot: children: seq[JamboneASTNode]
+    of jamRoot: children*: seq[JamboneASTNode]
     of jamBlock:
       blockName: string
       contents: JamboneASTNode
@@ -54,28 +69,60 @@ type
       variable: JamboneASTNode
     of jamVariable:
       varName: string
-      varValue: Showable
+      varValue: JsonNode
     of jamText:
       text: string
 
 # TODO render partials
 # proc renderPartial*
 
-proc isToken(str: string): bool =
+proc newKeyword*(tokenStr: string): Token =
+  return Token(kind: TokenKind.jamKeyword, keyword: parseEnum[KeywordTokenKind](tokenStr))
+
+proc isValidKeyword(str: string): bool =
   result = false
 
-  for t in Token:
+  # TODO make this hashmap.
+
+  for t in KeywordTokenKind:
     if $t == str:
       return true
 
-proc anyTokenContains(str: string): bool =
+  # Check for identifier
+
+  # TODO Check for literals
+  # TODO Check for comments
+
+
+proc anyKeywordContains(str: string): bool =
   result = false
 
-  for t in Token:
+  for t in KeywordTokenKind:
     if ($t).contains(str):
       return true
 
   # return list of candidates?
+
+proc findExpressionToken(region: StringStream): tuple[pos: int, token: Option[Token]] =
+  result = (0, none(Token))
+  var tokenStr = ""
+
+  var tokenPos = region.getPosition()
+  var c = region.readChar()
+
+  while c != '\0':
+    # TODO optimize - only check against list of valid candidate tokens
+    #     let candidates: seq = StartExpression[tokenPos],
+    tokenStr.add(c)
+    # TODO needs escaping
+    if tokenStr == $KeywordTokenKind.StartExpression:
+      return (pos: tokenPos, token: some(newKeyword(tokenStr)))
+    if c != '{':
+      tokenStr = ""
+      tokenPos = region.getPosition()
+
+    c = region.readChar()
+
 
 proc findToken(region: StringStream): tuple[pos: int, token: Option[Token]] =
   result = (0, none(Token))
@@ -85,33 +132,164 @@ proc findToken(region: StringStream): tuple[pos: int, token: Option[Token]] =
   var tokenPos = region.getPosition()
   var c = region.readChar()
 
-  while c != '\0':
-    tokenStr.add(c)
-    if not anyTokenContains(tokenStr):
-      tokenStr = ""
-      tokenPos = region.getPosition()
-      c = region.readChar()
-      continue
-
+  while true:
     # TODO optimize - only check against list of valid candidate tokens
     #     let candidates: seq = StartExpression[tokenPos],
+    if c == ' ' or c == '\0':
+      # TODO literals
+      # if c == '\'' or c == '"':
+      # string literal
+      # if c.isDigit
+      # number literal
+      # TODO comments
+      if isValidKeyword(tokenStr):
+        return (pos: tokenPos, token: some(newKeyword(tokenStr)))
 
-    if isToken(tokenStr):
-      return (pos: tokenPos, token: some(parseEnum[Token](tokenStr)))
+      elif validIdentifier(tokenStr):
+        return (pos: tokenPos, token: some(Token(kind: TokenKind.jamIdentifier, identifier: tokenStr)))
+
+      else:
+        if c == '\0':
+          break
+
+        tokenStr = ""
+        tokenPos = region.getPosition()
+
+    else:
+      tokenStr.add(c)
 
     c = region.readChar()
-    # tokenPos += 1
 
 iterator tokenizer*(cursor: StringStream): TokenOccurrence =
   # var curpos = 0
-  while isSome((var (pos, token) = findToken(cursor); token)):
-    yield (pos: pos, token: token.get())
+  # Are we tokenizing - somewhat of a mini optimization, don't want garbage tokens or to parse the HTML
+  var tokenizingExpression = false
+
+  while isSome((var (pos, token) = (if tokenizingExpression: findToken(cursor) else: findExpressionToken(cursor)); token)):
+    let tokenVal = token.get()
+    if tokenVal.kind == TokenKind.jamKeyword and tokenVal.keyword == KeywordTokenKind.StartExpression:
+      tokenizingExpression = true
+
+    elif tokenVal.kind == TokenKind.jamKeyword and tokenVal.keyword == KeywordTokenKind.EndExpression:
+      tokenizingExpression = false
+
+    yield (pos: pos, token: tokenVal)
     # curpos = pos
 
+proc tokenize*(source: string): Deque[TokenOccurrence] =
+  result = initDeque[TokenOccurrence]()
 
-proc parse*(source: string, tokens: seq[TokenOccurrence], context: Table[string, Showable]): JamboneASTNode =
-  result = JamboneASTNode(kind: jamRoot, children: @[])
+  var cursor = newStringStream(source)
 
+  # First phase - tokenizing
+  for tokenOccurrence in tokenizer(cursor):
+    result.addLast(tokenOccurrence)
+
+
+type ParserError = object
+  # case kind: ParserErrorKind
+  message: string
+  lineNumber: int
+  token: TokenOccurrence
+
+proc `$`(x: ParserError): string =
+  return &"Error: {x.message}\nLine number: {x.lineNumber}\n"
+
+type ParserContextObj = object
+    #stack: seq[JamboneASTNode]
+    # root: JamboneASTNode
+    lineNumber: int
+    #node: Option[JamboneASTNode]
+    activeExpression: seq[TokenOccurrence]
+    error: seq[ParserError]
+
+type ParserContext = ref ParserContextObj
+# parse("...", [start token, block token, end token], )
+
+proc parse*(currentNode: JamboneASTNode, source: StringStream, tokens: var Deque[TokenOccurrence], context: var ParserContext): JamboneASTNode =
+  if len(tokens) == 0:
+    return currentNode
+
+  echo currentNode.kind
+
+  # result = JamboneASTNode(kind: jamRoot, children: @[])
+  # let stack: seq[JamboneASTNode] = @[]
+  # let activeTokens: seq[TokenOccurrence] = @[]
+  let token = tokens.popFirst()
+
+  # set position to current token
+  source.setPosition(token.pos)
+
+  if token.token.kind != TokenKind.jamKeyword:
+    # TODO
+    return
+
+  case token.token.keyword:
+  of StartExpression:
+    if len(context.activeExpression) > 0:
+      context.error.add(ParserError(message: "Curly brackets not closed properly!", lineNumber: context.lineNumber, token: token))
+      return
+
+    let lineNumber = context.lineNumber
+    context.activeExpression.add(token)
+    # result = parse(source, tokens, context)
+    # if result.isNil:
+    #   context.error.add(ParserError(message: "Invalid expression:", lineNumber: lineNumber, token: token))
+    #   return
+
+    # Starting a node, pop the next token
+  of EndExpression:
+    if len(context.activeExpression) < 2:
+      context.error.add(ParserError(message: "Empty expression or missing starting bracket", lineNumber: context.lineNumber, token: token))
+      return
+
+    context.activeExpression.setLen(0)
+    # result = nil
+    #node = JamboneASTNode(kind: )
+
+  of Show:
+    discard
+
+  of StartBlock:
+    # backtracking
+    if len(context.activeExpression) != 1:
+      context.error.add(ParserError(message: "Invalid syntax: Block", lineNumber: context.lineNumber, token: token))
+      return
+
+    context.activeExpression.add(token)
+    result = JamboneASTNode(kind: JamboneASTKind.jamBlock, blockName: "")
+
+  of EndBlock:
+    discard
+
+  of IfBlock:
+    discard
+
+  of ElseBlock:
+    discard
+
+  of EndIfBlock:
+    discard
+
+
+  return parse(parse(result, source, tokens, context), source, tokens, context)
+
+proc newParseTree*(source: string, tokens: var Deque[TokenOccurrence], view: Table[string, Showable]): JamboneAstNode =
+  var context = ParserContext(
+    lineNumber: 0,
+    # view: view,
+    activeExpression: @[],
+    error: @[]
+  )
+  # context.activeExpression = newSeq[var TokenOccurrence]()
+
+  # Create the root node
+  let root = JamboneASTNode(kind: JamboneASTKind.jamRoot, children: @[])
+  let stream = newStringStream(source)
+  result = parse(root, stream, tokens, context)
+
+  if len(context.error) > 0:
+    echo context.error
 
 proc eval*(ast: JamboneASTNode): string =
   result = ""
@@ -125,14 +303,8 @@ proc eval*(ast: JamboneASTNode): string =
 let defaultConfig = %*{}
 
 # Output to string
-proc render*(source: string, context: Table[string, Showable], config: JsonNode = defaultConfig): string =
-  var tokens = newSeq[TokenOccurrence]()
-
-  var cursor = newStringStream(source)
-
-  # First phase - tokenizing
-  for tokenOccurrence in tokenizer(cursor):
-    tokens.add(tokenOccurrence)
+proc render*(source: string, view: Table[string, Showable], config: JsonNode = defaultConfig): string =
+  var tokens = tokenize(source)
 
   echo tokens
   # Second phase -
--- tests/test1.nim
+++ tests/test1.nim
@@ -13,15 +13,18 @@ suite "block parsing - phase 1":
 
     var (startPos, startToken) = tokens[0]
     check startPos == 0
-    check startToken == Token.StartExpression
+    check startToken.kind == TokenKind.jamKeyword
+    check startToken.keyword == KeywordTokenKind.StartExpression
 
     let (builtinPos, builtinToken) = tokens[1]
     check builtinPos == 3
-    check builtinToken == Token.IfBlock
+    check builtinToken.kind == TokenKind.jamKeyword
+    check builtinToken.keyword == KeywordTokenKind.IfBlock
 
     var (endPos, endToken) = tokens[2]
     check endPos == 6
-    check endToken == Token.EndExpression
+    check endToken.kind == TokenKind.jamKeyword
+    check endToken.keyword == KeywordTokenKind.EndExpression
 
     # cursor = newStringStream("{{ else }}")
 
